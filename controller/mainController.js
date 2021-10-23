910fdf7b42809ccfdad2e1233ebfea976fa73967
mlcas <martincastro88@hotmail.com>
2021-10-23 14:13:51 -0700

    Agregando dinamismo al Slider

--- controller/mainController.js
+++ controller/mainController.js
@@ -2,8 +2,17 @@ const path = require("path");
 
 let mainController = {
     index: (req, res) => {
-     
-     res.render("index")
+     const sliderInfo = [
+         {class: "item-a",img:"Imagen-principal.jpg"},
+         {class: "item-a",img:"nike-futbol-women.jpg"},
+         {class: "item-b",img:"Real-Madrid.jpg"},
+         {class: "item-c",img:"botas-adidas.jpg"},
+         {class: "item-d",img:"Puma-camisa.jpg"},
+         {class: "item-e",img:"R.jpg"},
+         {class: "item-a",img:"img-slider-women.jpg"},
+
+     ]
+     res.render("index",{sliderInfo})
     }
 }
 
