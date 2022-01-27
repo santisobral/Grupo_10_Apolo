c816f7466f4e05c933288a4b2d5730120b576bb4
mlcas <martincastro88@hotmail.com>
2022-01-27 14:51:55 -0800

    paginado de la api realizado en grupo

--- controller/api/productApiController.js
+++ controller/api/productApiController.js
@@ -3,14 +3,19 @@ const db = require("../../database/models");
 
 let productApiController = {
     'list': (req, res) => {
+       
         let products = db.Product.findAll({
             raw:true,// te trae directamente el array dentro de lo que te trae la base de datos
            // include: ['category','size'] NO SE SI VA ESTO, CREO QUE USANDO  EL PRODUCT.RELATION YA ESTA
-           attributes: ['id','name','description','category_id'] // CON ESTO NO NECESITO BORRAR CON EL DELETE
+           attributes: ['id','name','description','category_id'], // CON ESTO NO NECESITO BORRAR CON EL DELETE
+          
         })
         let categories = db.Category.findAll({
            raw:true
         })
+
+       
+
         Promise.all([products,categories])
         .then(([products,categories]) => {
 
@@ -21,6 +26,7 @@ let productApiController = {
                 categoriesCount.push(0)
               
             })
+
             products.forEach((product) => {
                 categoriesCount[product.category_id - 1] = categoriesCount[product.category_id - 1] + 1 // le pones el menos uno porque al array arranca de la posicion 0, la parte de la izquierda es la que acumula y se posiciona dentro de lo que le corresponde en el array
             // y cuando estas recorriendo el array en la posicion 0 como product.category es 1 le resta un 1 para estar en la posicion 0 y le sumas 1 para que te cuente 1 elemento en esa posicion
@@ -32,30 +38,45 @@ let productApiController = {
             }
            // res.send(products)
             products.forEach(product => {
-                // delete product.price;// aca eliminamos informacion que a React no le sirve
-                // delete product.brand;
-                // delete product.size_id;
-                // delete product.category_id;
-                // delete product.discount;
-                // delete product.image;
-                // delete product.gender;
-                // delete product.deleted
+               
                 product.relation = [ // aca le agregamos al objeto este array con la relaciones
                     "category_id",
                     "size_id"
                 ];
                 product.detailUrl = 'http://localhost:3030/api/product/' + product.id
             });
+           
             return res.status(200).json({
                 count: products.length,
                 countByCategory: countByCategoryToSend,
                 categoriesCount: categories.length,
                 products: products,
+              
                 status: 200
             })
            
             })
     },
+    paginate: (req, res) => {
+        const { page, size } = req.query;
+        let pagina = parseInt(page);
+        let productPerPage = parseInt(size);
+        db.Product.findAll({
+            raw: true,
+            attributes: ['name'],
+            limit : productPerPage,
+            offset : pagina * productPerPage
+        }
+
+        ).then(product => {
+            let respuesta = {
+                products: product,
+                status: 200
+            }
+            res.status(200).json(respuesta);
+        })
+    },
+    
     'detail': (req, res) => {
         db.Product.findByPk(req.params.id,{
             include: ['category','size']})
--- routes/api/productApiRouter.js
+++ routes/api/productApiRouter.js
@@ -4,6 +4,8 @@ const productApiController = require('../../controller/api/productApiController'
 
 // Devuelve todos los productos
 router.get("/",productApiController.list);
+// Devuelve los productos por pagina
+router.get("/paginate",productApiController.paginate);
 // Devuelve el detalle de un producto
 router.get('/:id', productApiController.detail);
 
